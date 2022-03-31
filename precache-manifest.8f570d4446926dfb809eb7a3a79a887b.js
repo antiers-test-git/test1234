eeac844f77832cae6e0fd480e296adc3760f0f14
mdshaiyad <mohammad.shaiyad@antiersolutions.com>
2022-03-31 21:03:19 +0530

    first commit

---
+++ precache-manifest.8f570d4446926dfb809eb7a3a79a887b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d296cd1c2f3db74f2d2cd647fcecc10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4157b563e71aa3dbe507",
+    "url": "/static/css/2.680df894.chunk.css"
+  },
+  {
+    "revision": "05157047932a6ea9109d",
+    "url": "/static/css/main.caeeb554.chunk.css"
+  },
+  {
+    "revision": "4157b563e71aa3dbe507",
+    "url": "/static/js/2.f2de61bd.chunk.js"
+  },
+  {
+    "revision": "dffa8a9855ef8488dc132f2e50e34a86",
+    "url": "/static/js/2.f2de61bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05157047932a6ea9109d",
+    "url": "/static/js/main.964915d4.chunk.js"
+  },
+  {
+    "revision": "b8ffb0637329909670a0",
+    "url": "/static/js/runtime-main.f94c535b.js"
+  },
+  {
+    "revision": "745cdc398c249fa6f0c2b50181ae5db5",
+    "url": "/static/media/active.745cdc39.png"
+  },
+  {
+    "revision": "e7c0d836cb77b2c08afa0e9cdf5ec63f",
+    "url": "/static/media/icon_transSuccess.e7c0d836.svg"
+  },
+  {
+    "revision": "a44ecb6e52f39364e493a5b64f9b0b3c",
+    "url": "/static/media/inactive.a44ecb6e.png"
+  },
+  {
+    "revision": "95cdbf14a69b342fcf029977ef5b2b82",
+    "url": "/static/media/logo_dark.95cdbf14.svg"
+  },
+  {
+    "revision": "6bb17460a97548d9fff95c60637e0767",
+    "url": "/static/media/logo_light.6bb17460.svg"
+  }
+]);
